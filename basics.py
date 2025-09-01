c77121899d9064931e1abef19187898a72158003
Sai Vinaya <kumbamsaivinayareddy@gmail.com>
2025-09-01 23:16:29 +0530

    new

---
+++ basics.py
@@ -0,0 +1,24 @@
+print("hello world!")
+a = 5
+b = 10
+c = a + b   #
+print(c)
+#using python as a calculator
+#addition
+print(2+5)
+print(1+2.8)
+#subtraction
+print(5-2)
+#multiplication
+print(5*2)
+#Division
+print(6/3)
+
+###comments in python
+#Comments are lines in a code file that are not executed by the Python interpreter. They are used to explain what the code is doing, making it easier to understand. In Python, comments start with the hash symbol (#).
+#importance:
+#clarity,Maintenance,self-Reference
+# python teminologies
+import keyword
+print("the list of keywords:")
+print(keyword.kwlist)
