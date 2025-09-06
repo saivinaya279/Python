b85065f105339d12c9623559beda5dcaf23698d2
Sai Vinaya <kumbamsaivinayareddy@gmail.com>
2025-09-06 13:59:17 +0530

    commit

---
+++ operators.py
@@ -0,0 +1,54 @@
+# Operators in Python
+# Operators Type
+# Arithmetic operator:+, -, *, /,%
+# Relational operator:<,<=,>,>=,==,!,!=
+# Logical operator:AND, OR, NOT
+# Bitwise operator:&, |,<<,>>,-,^
+# Assignment operator:=,+=,-=,^ * =,\%=
+# identity operators:is,is not 
+# Membership operators:in ,not in
+
+ 
+# OPERATORS: These are the special symbols. Eg- + , * , /, etc.
+# OPERAND: It is the value on which the operator is applied.
+# Arithmetic Operators:
+# Python Arithmetic operators are used to perform basic mathematical operations like addition, subtraction, multiplication and division.
+a=15
+b=4
+# addition
+print(a+b)
+print(a-b)  # Subtraction
+print(a*b)# Multiplication
+print(a/b)# Division
+print(a//b)# Floor Division
+print(a%b) # Modulus
+print(a**b)#Exponentiation
+# comparison operators or relational operators:<,<=,>,>=,==,!,!=
+# In Python Comparison of Relational operators compares the values. It either returns True or False according to the condition.
+a = 13
+b = 33
+print(a>b)
+print(a<b)
+print(a<=b)
+print(a!=b)
+print(a>=b)
+# Logical Operators:AND, OR, NOT
+# It is used to combine conditional statements.
+print("/n")
+a = True
+b = False
+print(a and b)#True if both statements are the same
+print(a or b)#True if one of the statements is true.
+print(not a)#Reverse the result
+# Comparison Operators
+#  Input two numbers and check if the first is greater than the second.
+a=int(input())
+b=int(input())
+print(a>b)
+# : Input two strings and check if they are equal
+s=input()
+s1=input()
+print(s==s1)
+# Input an age and check if it is greater than or equal to 18
+age=int(input())
+print(age>=18)
