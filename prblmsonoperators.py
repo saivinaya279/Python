88f7d63b7b43b8c92a612008c1b6924fe539ee2d
Sai Vinaya <kumbamsaivinayareddy@gmail.com>
2025-09-06 14:01:58 +0530

    commit

---
+++ prblmsonoperators.py
@@ -0,0 +1,54 @@
+# Arithmetic Operators 
+# 1. Take two integers as input and print their sum, difference, product, and quotient.
+a=15
+b=4
+print(a+b)
+print(a-b)  
+print(a*b)# 
+print(a/b)# 
+print(a//b)#n
+print(a%b) 
+print(a**b)
+# Input a number and print its square and cube using **. 
+c=int(input())
+d=c**2
+e=c**3
+print(d)
+print(e)
+print("/n")
+# Input a number and print the remainder when divided by 7.
+d=int(input())
+f=d%7
+print(f)
+# Input two floats and print the result of dividing the first by the second with 2 decimal places. 
+c=int(input())
+d=int(input())
+e=c/d
+print(e)
+# Comparison Operators
+#  Input two numbers and check if the first is greater than the second.
+a=int(input())
+b=int(input())
+print(a>b)
+# : Input two strings and check if they are equal
+s=input()
+s1=input()
+print(s==s1)
+# Input an age and check if it is greater than or equal to 18
+age=int(input())
+print(age>=18)
+# logical operators
+# Input two Boolean values (True/False) and print their and, or, not results.
+c=bool(int(input()))
+d=bool(int(input()))
+print(c and b)
+print(c or d)
+print(not d)
+# . Input a number and check if it lies between 10 and 20 (inclusive) using logical operators.
+number=int(input())
+print(number>=10 and number<=20)
+# Input three numbers and check if all are positive.
+a=int(input())
+b=int(input())
+c=int(input())
+print(a>0 and b>0 and c>0)
