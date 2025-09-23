35c41878314881c172480d87a2d37e9b5373f4a2
Sai Vinaya <kumbamsaivinayareddy@gmail.com>
2025-09-23 23:06:42 +0530

    commit

--- patterns.py
+++ patterns.py
@@ -9,18 +9,35 @@ c=len(li[0])
 for i in range(r):
     for j in range (c):
         print(li[i][j],end=" ")
+        
     print()
+# Print a solid square of * with n rows and n columns
+
 r=3
 c=3
 for i in range(r):
     for j in range(c):
         print("*",end=" ")
     print()
+#  stars(* ),with (_)
 r=int(input())
 c=int(input())
 for i in range(r):
     for j in range(c):
-        print("*",end=" ")
+        print("*",end="_")
+    print()
+    """ *_*_*_*_*
+         *_*_*_*_*
+         *_*_*_*_*
+         *_*_*_*_*   
+         basically question  write a code to  is not to print the (_)at the end  like above"""
+r=int(input())
+c=int(input())
+for i in range(r):
+    for j in range(c):
+        print("*",end="")
+        if j !=c-1:
+             print("-",end="")
     print()
     
     
