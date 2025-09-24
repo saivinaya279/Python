8e4685f736f0aaa58442620dc02041ca9c05a4d4
Sai Vinaya <kumbamsaivinayareddy@gmail.com>
2025-09-24 22:22:16 +0530

    commit

--- patterns.py
+++ patterns.py
@@ -1,58 +1,63 @@
-print("hello" ,end=" ")
-print("vinaya")
-li=["hello","boom"]
-for i in range(len(li)):
-    print(li[i])
-li=[["*","*","*"],["*","*","*"],["*","*","*"]]
-r=len(li)
-c=len(li[0])
-for i in range(r):
-    for j in range (c):
-        print(li[i][j],end=" ")
+# print("hello" ,end=" ")
+# print("vinaya")
+# li=["hello","boom"]
+# for i in range(len(li)):
+#     print(li[i])
+# li=[["*","*","*"],["*","*","*"],["*","*","*"]]
+# r=len(li)
+# c=len(li[0])
+# for i in range(r):
+#     for j in range (c):
+#         print(li[i][j],end=" ")
         
-    print()
-# Print a solid square of * with n rows and n columns
+#     print()
+# # Print a solid square of * with n rows and n columns
 
-r=3
-c=3
-for i in range(r):
-    for j in range(c):
-        print("*",end=" ")
-    print()
-#  stars(* ),with (_)
-r=int(input())
-c=int(input())
-for i in range(r):
-    for j in range(c):
-        print("*",end="_")
-    print()
-    """ *_*_*_*_*
-         *_*_*_*_*
-         *_*_*_*_*
-         *_*_*_*_*   
-         basically question  write a code to  is not to print the (_)at the end  like above"""
-r=int(input())
-c=int(input())
-for i in range(r):
-    for j in range(c):
-        print("*",end="")
-        if j !=c-1:
-             print("-",end="")
-    print()
+# r=3
+# c=3
+# for i in range(r):
+#     for j in range(c):
+#         print("*",end=" ")
+#     print()
+# #  stars(* ),with (_)
+# r=int(input())
+# c=int(input())
+# for i in range(r):
+#     for j in range(c):
+#         print("*",end="_")
+#     print()
+#     """ *_*_*_*_*
+#          *_*_*_*_*
+#          *_*_*_*_*
+#          *_*_*_*_*   
+#          basically question  write a code to  is not to print the (_)at the end  like above"""
+# r=int(input())
+# c=int(input())
+# for i in range(r):
+#     for j in range(c):
+#         print("*",end="")
+#         if j !=c-1:
+#              print("-",end="")
+#     print()
     
     
-# Print a solid rectangle of * with n rows and n columns
-n = 4
-m = 6
+# # Print a solid rectangle of * with n rows and n columns
+# n = 4
+# m = 6
 
-for i in range(n):
-    for j in range(m):
-        print("*", end=" ")
-    print()
+# for i in range(n):
+#     for j in range(m):
+#         print("*", end=" ")
+#     print()
 
-n = 5
-# Print a right-angled triangle of * (increasing).
-for i in range(1, n+1):
+# n = 5
+# # Print a right-angled triangle of * (increasing).
+# for i in range(1, n+1):
+#     for j in range(i):
+#         print("*", end=" ")
+#     print()
+n=5
+for i in range(n,0,-1):
     for j in range(i):
-        print("*", end=" ")
+        print("*",end=" ")
     print()
