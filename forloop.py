9647654532b3d3e125c75e73513dd1980dfe3434
Sai Vinaya <kumbamsaivinayareddy@gmail.com>
2025-09-29 15:40:14 +0530

    commit

--- forloop.py
+++ forloop.py
@@ -118,6 +118,15 @@ for i in range(1,n+1):
     for j in range(i):
         print(i,end=" ")
     print()
+# Palindrom number
+n=int(input())
+for i in  range(1,n+1):
+    print(" "*(n-i),end="")
+    for j in range(i,0,-1):
+        print(j, end="")
+    for j in range(2,i+1):
+        print(j, end="")
+    print()
 
 
 
