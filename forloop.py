ed9a2eacba5d288366bd2c1f0e5b39418a4fea39
Sai Vinaya <kumbamsaivinayareddy@gmail.com>
2025-09-30 20:41:20 +0530

    commit

--- forloop.py
+++ forloop.py
@@ -140,6 +140,18 @@ class Solution:
         return expectedSum-actualSum
         
     
+    # Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+# Input: nums = [3,0,1]
+
+# Output: 2
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n=len(nums)
+        expectedSum=n*(n+1)//2
+        actualSum=sum(nums)
+        return expectedSum-actualSum
+        
+    
     
     
 
