2715a597bd4273e4e2186e8810c06cc7aaae2117
Sai Vinaya <kumbamsaivinayareddy@gmail.com>
2025-10-03 21:58:55 +0530

    Update forloop.py

--- forloop.py
+++ forloop.py
@@ -150,6 +150,36 @@ class Solution:
         expectedSum=n*(n+1)//2
         actualSum=sum(nums)
         return expectedSum-actualSum
+# Print even numbers from 1 to 20.
+i=0
+for i in range(0,21,2):
+    print(i)
+# Print all multiples of 5 from 1 to 50.
+i=0
+for i in range(0,51,5):
+    print(i)
+# 
+for i in range(1,51):
+    if i%5==0:
+        print(i)
+# Find the sum of numbers from 1 to N (take N as input).
+n=int(input())
+total=0
+for i in range(1,n+1):
+    total+=i
+print(total)
+# Find the product (multiplication) of numbers from 1 to N.
+n=int(input())
+prod=0
+for i in range(1,n+1):
+    prod*=i
+print(total)
+# Find the sum of numbers from 1 to N (take N as input).
+n=int(input())
+sum=0
+for i in range(1,n+1):
+    sum=sum+i
+print(sum)
         
     
     
