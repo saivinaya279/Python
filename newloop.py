2c511232811a3d6a0531a716e78b2e3456ee4833
Sai Vinaya <kumbamsaivinayareddy@gmail.com>
2025-10-10 23:51:31 +0530

    commit

--- newloop.py
+++ newloop.py
@@ -158,3 +158,14 @@ if is_pangram:
 else:
     print("Not Pangram")
 
+
+"""1 
+1 2 
+1 2 3 
+1 2 3 4 
+1 2 3 4 5 """
+n=5
+for i in range(1,n+1):
+    for j in range(i):
+        print(j+1,end=" ")
+    print()
