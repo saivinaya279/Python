b0b4f4c5a21b7fe2dd0082a8bde3c7eb76ef1edd
Sai Vinaya <kumbamsaivinayareddy@gmail.com>
2025-10-11 23:02:25 +0530

    commit

--- newloop.py
+++ newloop.py
@@ -169,3 +169,14 @@ for i in range(1,n+1):
     for j in range(i):
         print(j+1,end=" ")
     print()
+    
+    
+# Count digits of a number
+n = int(input())
+count = 0
+
+while n > 0:
+    count += 1
+    n //= 10
+
+print( count)
