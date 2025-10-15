9a744ec26e94f4296c872508b5eb1a93ff8bb743
Sai Vinaya <kumbamsaivinayareddy@gmail.com>
2025-10-15 22:36:50 +0530

    commit

--- stringproblems.py
+++ stringproblems.py
@@ -42,6 +42,42 @@ else:
 s=" h e l l o"
 result=""
 for ch in s:
-    if ch!=" ":
-        result+= ch
+    if ch!=" ": # if  not space then 
+        result+= ch # add to the result
 print(result)
+
+s="I love Python"
+b="python"
+c="java"
+for ch in s:
+    if b==len(s):
+        b=c
+    print(b)
+"""Given a string and a word, check whether the string starts with or ends with that particular word.
+
+Example:
+Input: "Hello world", word = "Hello"
+Output: String starts with 'Hello'
+
+Input: "Hello world", word = "world"
+Output: String ends with 'world'"""
+s=input()
+word=input()
+start= True
+for i in range(len(word)):
+    if s[i]!=word[i]:
+        start=False
+        break
+end= True
+for i in range(1,len(word)+1):
+    if s[-i]!=word[-i]:
+        end=False
+        break
+if start:
+    print("start")
+elif end:
+    print("end")
+else:
+    print("not ")
+
+        
