b24d8aa140e51474078fcf65799f6caf188d1ba5
Sai Vinaya <kumbamsaivinayareddy@gmail.com>
2025-10-23 22:20:22 +0530

    commit

---
+++ lists.py
@@ -0,0 +1,58 @@
+# What is a List?
+
+# A list is an ordered, mutable (changeable) collection that can hold different data types.
+
+fruits = ["apple", "banana", "cherry"]
+numbers = [10, 20, 30, 40]
+mixed = ["hello", 5, True, 3.14]
+# 2️ Accessing Elements
+
+# Python lists are zero-indexed (first element → index 0).
+
+# You can also use negative indexing (last element → index -1).
+
+fruits = ["apple", "banana", "cherry"]
+print(fruits[0])   # apple
+print(fruits[-1])  # cherry
+# Modifying & Deleting
+# 
+# Lists are mutable — you can change or remove elements.
+
+fruits[1] = "mango"      # update
+print(fruits)            # ['apple', 'mango', 'cherry']
+
+del fruits[2]            # delete using del
+print(fruits)    
+# List creation + indexing.
+
+# Example:
+# Input: nums = [1,2,1]
+# Output: [1,2,1,1,2,1]
+
+nums = [1,2,1]
+ans = nums + nums
+print(ans)
+# Create & Print
+colors = ["red", "green", "blue", "yellow"]
+print(colors[0])    
+print(colors[-1])   
+# replace
+animals = ["cat", "dog", "rabbit", "tiger"]
+animals[1] = "lion"
+print(animals)
+# Access Using Negative Index
+numbers = [10, 20, 30, 40, 50]
+print(numbers[-2])  
+print(numbers[-1])  
+# List Slicing
+movies = ["Inception", "Avatar", "Titanic", "Joker", "RRR", "KGF"]
+print(movies[1:4])
+# ️Delete Element
+fruits = ["apple", "banana", "cherry", "mango"]
+del fruits[1]
+print(fruits)
+# del list[index] removes the element at that index.
+
+#  Length of List
+subjects = ["Math", "Science", "English", "History", "Civics"]
+print(len(subjects))
