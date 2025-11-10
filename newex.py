d23ab2a882229942853ac2926ce54685198c5d2b
Sai Vinaya <kumbamsaivinayareddy@gmail.com>
2025-11-10 23:37:47 +0530

    commit

---
+++ newex.py
@@ -0,0 +1,15 @@
+#  Smallest element
+
+a=[3,5,2,8,1]
+min_num=a[0]
+for i in a:
+    if i<min_num:
+        min_num=i
+print(min_num)
+""" nums=[1,2,3,4,5]
+output=[5,4,3,2,1]"""
+nums = [1, 2, 3, 4, 5]
+rev = []
+for i in nums:
+    rev.insert(0, i)
+print(rev)
