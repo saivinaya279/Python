095567d31ae96f3a013fe0a4c399157fd3019db0
Sai Vinaya <kumbamsaivinayareddy@gmail.com>
2025-11-14 19:51:03 +0530

    commit

--- newex.py
+++ newex.py
@@ -30,3 +30,98 @@ for i in range(n, 0, -1):
     for j in range(i, 0, -1):
         print(j, end=" ")
     print()
+    # Row number repeated in each row:
+
+n = 5
+for i in range(1, n+1):
+    for j in range(i):
+        print(i, end=" ")
+    print()
+"""3. Print pattern of alphabets
+
+
+
+A
+A B
+A B C
+A B C D
+A B C D E
+n = 5"""
+for i in range(1, n + 1):       # Controls number of rows (1 to 5)
+    for j in range(65, 65 + i): # Controls which alphabets to print in each row
+        print(chr(j), end=" ")
+    print()
+"""# inverted alphabetical trirangle
+A B C D E 
+A B C D 
+A B C 
+A B 
+A"""
+n = 5
+for i in range(n, 0, -1):
+    for j in range(65, 65 + i):
+        print(chr(j), end=" ")
+    print()
+#row repeated alphabets 
+n = 5
+for i in range(65, 65 + n):
+    for j in range(65, i + 1):
+        print(chr(i), end=" ")
+    print()
+"""65 
+65 66 
+65 66 67 
+65 66 67 68"""
+s = "ABCD"
+
+for i in range(1, len(s) + 1):
+    for j in range(i):
+        print(ord(s[j]), end=" ")
+    print()# Row number repeated in each row:
+
+n = 5
+for i in range(1, n+1):
+    for j in range(i):
+        print(i, end=" ")
+    print()
+"""3. Print pattern of alphabets
+
+
+
+A
+A B
+A B C
+A B C D
+A B C D E
+n = 5"""
+for i in range(1, n + 1):       # Controls number of rows (1 to 5)
+    for j in range(65, 65 + i): # Controls which alphabets to print in each row
+        print(chr(j), end=" ")
+    print()
+"""# inverted alphabetical trirangle
+A B C D E 
+A B C D 
+A B C 
+A B 
+A"""
+n = 5
+for i in range(n, 0, -1):
+    for j in range(65, 65 + i):
+        print(chr(j), end=" ")
+    print()
+#row repeated alphabets 
+n = 5
+for i in range(65, 65 + n):
+    for j in range(65, i + 1):
+        print(chr(i), end=" ")
+    print()
+"""65 
+65 66 
+65 66 67 
+65 66 67 68"""
+s = "ABCD"
+
+for i in range(1, len(s) + 1):
+    for j in range(i):
+        print(ord(s[j]), end=" ")
+    print()
