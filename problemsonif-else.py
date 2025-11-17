c94706c1ec89aae0566974625b12afb96b29b914
Sai Vinaya <kumbamsaivinayareddy@gmail.com>
2025-11-17 18:00:00 +0530

    Code updated

--- problemsonif-else.py
+++ problemsonif-else.py
@@ -307,3 +307,14 @@ for ch in text:
     if ch in vowels:
         count += 1
 print(count)
+
+Largest of Three Numbers
+a = int(input())
+b = int(input())
+c = int(input())
+if a >= b and a >= c:
+    print(a)
+elif b >= c:
+    print(b)
+else:
+    print(c)
