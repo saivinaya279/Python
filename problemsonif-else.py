1683bc6affcd43c1b6751b10d5d5a264b5453f3b
Sai Vinaya <kumbamsaivinayareddy@gmail.com>
2025-11-29 18:00:00 +0530

    Code updated

--- problemsonif-else.py
+++ problemsonif-else.py
@@ -390,3 +390,14 @@ if sorted(s1)==sorted(s2):
     print("Anagram")
 else:
     print("Not anagram")
+    
+# Print Prime Numbers in a Range
+start = int(input("Start: "))
+end = int(input("End: "))
+for num in range(start, end+1):
+    if num > 1:
+        for i in range(2, num):
+            if num % i == 0:
+                break
+        else:
+            print(num)
