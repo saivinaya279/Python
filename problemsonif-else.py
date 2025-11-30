0a80315d14838fd77a4f849026b9902ade7d0804
Sai Vinaya <kumbamsaivinayareddy@gmail.com>
2025-11-30 23:54:41 +0530

    file added

--- problemsonif-else.py
+++ problemsonif-else.py
@@ -409,7 +409,7 @@ for i in range(1, n+1):
     fact*=i
 print(fact)
 
-Check if a number is even or odd
+# Check if a number is even or odd
 num = int(input("Enter a number: "))
 if num % 2 == 0:
     print("Even")
