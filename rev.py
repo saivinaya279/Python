6a1746338af9996757257e435d6a30e853c82777
Sai Vinaya <kumbamsaivinayareddy@gmail.com>
2025-12-08 12:00:00 +0530

    commit to 2

--- rev.py
+++ rev.py
@@ -123,4 +123,12 @@ while n>0:
         count+=1
     n=n//10
 print(count)
-        
+n = int(input())
+rev = 0
+
+while n > 0:
+    digit = n % 10
+    rev = rev * 10 + digit
+    n = n // 10
+
+print(rev)
