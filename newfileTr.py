3b836eb64fd068aec77cf1d0eb5ffe6e1cba9c9e
Sai Vinaya <kumbamsaivinayareddy@gmail.com>
2025-12-12 23:38:24 +0530

    file added

---
+++ newfileTr.py
@@ -0,0 +1,21 @@
+#identifiers
+name="python"
+print(name)
+A="python"
+print(A)
+"""rules
+#usage:we can declare a identifier with a-zz,A-z,
+# but we cannot stat with a number or special characters lie eg:_,@# etc:
+#1person :it wont work
+and we can't use reserve keywords, (ex:long,for if ),we can't start with them
+# It is case sensitive"""
+#variables
+"""Its is a container to store values
+# """
+a=10
+print(type(a))
+print(id(a))
+#reserve keyword
+import keyword
+print(keyword.kwlist)
+
