8203c3b3e1827c22314b118e9802a2954de6da5d
Sai Vinaya <kumbamsaivinayareddy@gmail.com>
2025-12-26 12:00:00 +0530

    file updated

--- tuples.py
+++ tuples.py
@@ -102,3 +102,82 @@ t3 = t1 + t2
 print(t3)
 # Repeat a tuple 3 times.
 
+t = (5, 10)
+print(t * 3)
+# Check if 30 exists in a tuple.
+
+t = (10, 20, 30, 40)
+if 30 in t:
+    print("yes")
+else:
+    print("NO")
+# Find length of a tuple
+t = (1, 2, 3, 4, 5)
+print(len(t))
+# Count how many times 2 appears.
+
+t = (1, 2, 3, 2, 4, 2)
+print(t.count(2))
+# Find maximum and minimum elements
+t = (4, 7, 1, 9, 3)
+print(max(t))
+print(min(t))
+# Print all elements greater than 25.
+
+t = (10, 20, 30, 40, 50)
+for i in t:
+    if i > 25:
+        print(i)
+# Create a tuple of 5 city names and print it.
+cities = ("Vizag", "Hyderabad", "Bangalore", "Chennai", "Mumbai")
+print(cities)
+# Create a tuple with numbers from 1 to 10 and print the first element.
+numbers = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+print (numbers[0])
+
+# Print the last element of a tuple (10, 20, 30, 40, 50).
+t = (10, 20, 30, 40, 50)
+print(t[-1])
+# Print elements from index 1 to 3 from the tuple (5, 10, 15, 20, 25).
+t = (5, 10, 15, 20, 25)
+print(t[1:4])
+nums = (10, 25, 3, 48, 15)
+# Find largest and smallest number without using min()/max()
+largest = nums[0]
+smallest = nums[0]
+
+for n in nums:
+    if n > largest:
+        largest = n
+    if n < smallest:
+        smallest = n
+
+print("Largest:", largest)
+print("Smallest:", smallest)
+# Check if two tuples are equal.
+t1 = (1, 2, 3)
+t2 = (1, 2, 3)
+t3 = (3, 2, 1)
+
+print(t1 == t2)  
+print(t1 == t3)
+# Combine two tuples and display sorted result.
+t1 = (9, 1, 4)
+t2 = (5, 7, 2)
+merged = tuple(sorted(t1 + t2))
+print(merged)
+
+ # Check Element Exists in Tuple
+t=(10,20,30,40)
+if 20 in t:
+    print("Element found")
+else:
+    print("Not found")
+
+# Nested Tuple Access
+t = (1,2,(3,4,5))
+print(t[2][1])  
+
+# Find Index of an Element
+t=(10,20,30,40)
+print(t.index(30))
