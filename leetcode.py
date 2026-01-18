13005df80ae75e5d29710bf9ea0199a9ade789c5
Sai Vinaya <kumbamsaivinayareddy@gmail.com>
2026-01-18 12:00:00 +0530

    file updated

--- leetcode.py
+++ leetcode.py
@@ -81,3 +81,14 @@ if __name__ == "__main__":
     solution.reverseString(s)
 
     print("Reversed String:", s)
+"""Given a string s which consists of lowercase or uppercase letters, return the length of the longest palindrome that can be built with those letters.
+
+Letters are case sensitive, for example, "Aa" is not considered a palindrome.
+
+ 
+
+Example 1:
+
+Input: s = "abccccdd"
+Output: 7
+Explanation: One longest palindrome that can be built is "dccaccd", whose length is 7."""
