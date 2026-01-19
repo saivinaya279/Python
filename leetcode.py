52fdef85145738c2a226b10abaaf30444a1b32f3
Sai Vinaya <kumbamsaivinayareddy@gmail.com>
2026-01-19 12:00:00 +0530

    file updated

--- leetcode.py
+++ leetcode.py
@@ -140,3 +140,12 @@ Example 3:
 Input: n = 14
 Output: false
 Explanation: 14 is not ugly since it includes the prime factor 7."""
+class Solution:
+    def isUgly(self, n: int) -> bool:
+        if n<=0:
+            return False
+        for i in [2,3,5]:
+            while n%i==0:
+                n//=i
+                
+        return n==1
