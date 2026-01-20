73e9dcb7975c91226f164a46c7e65e8d0af90b28
Sai Vinaya <kumbamsaivinayareddy@gmail.com>
2026-01-20 12:00:00 +0530

    file updated

--- leetcode.py
+++ leetcode.py
@@ -177,3 +177,20 @@ Output: 2
 Explanation:
 
 n = 2 since there are 2 numbers, so all numbers are in the range [0,2]. 2 is the missing number in the range since it does not appear in nums."""
+from typing import List
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        return list(set(range(n + 1)) - set(nums))[0]
+
+
+# -------- Main Execution --------
+if __name__ == "__main__":
+    nums = [3, 0, 1]   # sample input
+
+    solution = Solution()
+    result = solution.missingNumber(nums)
+
+    print("Input:", nums)
+    print("Missing Number:", result)
