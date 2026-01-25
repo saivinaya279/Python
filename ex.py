9a1a26d897bdc8858e0940ac30f79c15c730fbf0
Sai Vinaya <kumbamsaivinayareddy@gmail.com>
2026-01-25 12:00:00 +0530

    file updated

--- ex.py
+++ ex.py
@@ -1,11 +1,24 @@
-n = int(input("Enter n: "))
 
-for i in range(1, n + 1):
-    if i % 18 == 0:
-        print("FizzBuzz")
-    elif i % 6 == 0:
-        print("Fizz")
-    elif i % 9 == 0:
-        print("Buzz")
-    else:
-        print(i)
+def single_digit(a):
+    if n>=0 and n<=9:
+        return "Single_Digit"
+    return "not "
+n=int(input())
+print(single_digit(a))
+n=int(input())
+sum_i=0
+while n >0:
+    d=n%10
+    sum_i+=d
+    n//=10
+print(sum_i)
+def smallest_num(a,b,c):
+    if a<b and a<c:
+        return a
+    elif b<a and b<c:
+        return b
+    return c
+a=int(input())
+b=int(input())
+c=int(input())
+print(smallest_num(a,b,c))
