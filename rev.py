0b6ad465f28ed8b16852b91157f3612199144959
Sai Vinaya <kumbamsaivinayareddy@gmail.com>
2026-01-28 12:00:00 +0530

    file updated

--- rev.py
+++ rev.py
@@ -220,7 +220,7 @@ for i in range(5):
         print("*",end=" ")
     print()
     
-# Right-angled Triangle
+# Right-angled Triangle Pattern
 for i in range(1,6):
     for j in range(i):
         print("*",end=" ")
