c393850e62248ba764a5d179eb222bd9cc4128fb
Sai Vinaya <kumbamsaivinayareddy@gmail.com>
2026-01-29 12:00:00 +0530

    file updated

--- rev.py
+++ rev.py
@@ -250,3 +250,20 @@ for i in range(1, n+1):
     for j in range(1, i+1):
         print(j, end=" ")
     print()
+"""Same Number Triangle
+
+Output
+
+1
+2 2
+3 3 3
+4 4 4 4
+
+
+"""
+
+n = 4
+for i in range(1, n+1):
+    for j in range(i):
+        print(i, end=" ")
+    print()
