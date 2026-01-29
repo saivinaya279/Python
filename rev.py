3ab7a550326006d63275777e3db9edc0c7bb549f
Sai Vinaya <kumbamsaivinayareddy@gmail.com>
2026-01-29 12:00:00 +0530

    file updated

--- rev.py
+++ rev.py
@@ -234,3 +234,19 @@ for i in range(1,n+1):
     for k in range(i):
         print("*",end=" ")
     print()
+"""Number Triangle
+
+Output
+
+1
+1 2
+1 2 3
+1 2 3 4
+
+
+Code"""
+n = 4
+for i in range(1, n+1):
+    for j in range(1, i+1):
+        print(j, end=" ")
+    print()
