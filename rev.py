e4039540905550f99c52adf864ea13245be3c088
Sai Vinaya <kumbamsaivinayareddy@gmail.com>
2026-01-30 12:00:00 +0530

    file updated

--- rev.py
+++ rev.py
@@ -295,7 +295,14 @@ while left<right:
         right-=1
         
      
-         
+arr=list(map(int,input().split()))
+left=0
+right =len(arr)-1
+while left <right:
+    arr[left],arr[right]=arr[right],arr[left]
+    left+=1
+    right-=1
+print(arr)
 
         
         
