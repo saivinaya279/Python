909929dd337256bcc8b1d66054e6bc3b61471484
Sai Vinaya <kumbamsaivinayareddy@gmail.com>
2026-01-30 12:00:00 +0530

    file updated

--- rev.py
+++ rev.py
@@ -283,7 +283,18 @@ else:
          print("Not")
 
 
-         
+arr=list(map(int,input().split()))
+left=0
+right=len(arr)-1
+while left<right:
+    if arr[left]!=arr[right]:
+        print("not palindrome")
+    else:
+        print("palindrome")
+        left+=1
+        right-=1
+        
+     
          
 
         
