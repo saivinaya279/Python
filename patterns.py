f113a7e787791196d440e5c47713e4fdf1b8a1e2
Sai Vinaya <kumbamsaivinayareddy@gmail.com>
2026-02-01 12:00:00 +0530

    file updated

--- patterns.py
+++ patterns.py
@@ -148,3 +148,10 @@ for i in range(1, n+1):
     for j in range(1, i+1):  # start from 1, go till i
         print(j, end=" ")
     print()
+n=5
+for i in range(1,n+1):
+    for j in range(n-i):
+        print(" ",end=" ")
+    for k in range(i):
+        print("*",end=" ")
+    print()
