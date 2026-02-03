3b17c336744944520c755ce6da3c2d06032e23c3
Sai Vinaya <kumbamsaivinayareddy@gmail.com>
2026-02-03 12:00:00 +0530

    file updated

--- patterns.py
+++ patterns.py
@@ -133,32 +133,3 @@ for i in range(1, n+1):
         print(num, end=" ")
         num += 1
     print()
-# Palindromic number triangle:
-n = 5
-for i in range(1, n+1):
-    print(" "*(n-i), end="")
-    for j in range(i, 0, -1):
-        print(j, end="")
-    for j in range(2, i+1):
-        print(j, end="")
-    print()
-# numbers in increasing order starting
-n = 5
-for i in range(1, n+1):
-    for j in range(1, i+1):  # start from 1, go till i
-        print(j, end=" ")
-    print()
-n=5
-for i in range(1,n+1):
-    for j in range(n-i):
-        print(" ",end=" ")
-    for k in range(i):
-        print("*",end=" ")
-    print()
-n=5
-for i in range(n,0,-1):
-    for j in range(n-i):
-        print(" ",end=" ")
-    for k in range(2*i-1):
-        print("*",end=" ")
-    print()
