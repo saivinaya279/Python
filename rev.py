af1f0f5e03f34b7265737242491d3feecd5234c5
Sai Vinaya <kumbamsaivinayareddy@gmail.com>
2026-02-06 12:00:00 +0530

    file updated

--- rev.py
+++ rev.py
@@ -337,3 +337,9 @@ arr=list(map(int,input().split()))
 s1 = input()
 s2 = input()
 print("Anagram" if sorted(s1) == sorted(s2) else "Not Anagram")
+# Frequency of Each Character (Dictionary)
+s = input()
+freq = {}
+for ch in s:
+    freq[ch] = freq.get(ch, 0) + 1
+print(freq)
