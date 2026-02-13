66db6717ce6cc6a2965f45687a64c9997c1073db
Sai Vinaya <kumbamsaivinayareddy@gmail.com>
2026-02-13 12:00:00 +0530

    file updated

--- rev.py
+++ rev.py
@@ -467,3 +467,13 @@ for word in words:
         longest = word
 
 print(longest)
+# 20. Intersection Without Sets
+a = [1,2,3,4]
+b = [3,4,5,6]
+res = []
+
+for i in a:
+    if i in b and i not in res:
+        res.append(i)
+
+print(res)
