8095d4c7e41c0a6825c47a24a842fab7b79c5451
Sai Vinaya <kumbamsaivinayareddy@gmail.com>
2026-02-17 12:00:00 +0530

    file updated

--- functions.py
+++ functions.py
@@ -171,3 +171,11 @@ def factorial(n) -> int:
     return fact
 n=int(input())
 print(factorial(n))
+"""# Local Scope
+A variable created inside a function belongs to the local scope of that function, and can only be used inside that function.
+"""
+def myfunc():
+  x = 300
+  print(x)
+
+myfunc()
