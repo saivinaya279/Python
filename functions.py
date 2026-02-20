89ed229e7a198a84ab78adba5abc5be9c978b3ba
Sai Vinaya <kumbamsaivinayareddy@gmail.com>
2026-02-20 18:24:46 +0530

    Daily python practice

--- functions.py
+++ functions.py
@@ -216,3 +216,21 @@ def myfunc():
 myfunc()
 
 print(x)
+"""Global Keyword
+If you need to create a global variable, but are stuck in the local scope, you can use the global keyword.
+
+The global keyword makes the variable global."""
+# If you use the global keyword, the variable belongs to the global scope:
+
+def myfunc():
+  global x
+  x = 300
+
+myfunc()
+
+print(x)
+
+# Sum of 2 numbers using function
+def add(a, b):
+    return a + b
+print(add(5, 3))
