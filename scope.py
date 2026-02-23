54af067fb50b3533425617623283359d2ebe8e9c
Sai Vinaya <kumbamsaivinayareddy@gmail.com>
2026-02-23 10:00:00 +0530

    message

--- scope.py
+++ scope.py
@@ -1,2 +1,9 @@
 """Local Scope
 A variable created inside a function belongs to the local scope of that function, and can only be used inside that function."""
+# A variable created inside a function is available inside that function:
+
+def myfunc():
+  x = 300
+  print(x)
+
+myfunc()
