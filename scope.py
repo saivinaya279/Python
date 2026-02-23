70fc68226900c0f690de64cc07f8f1d0106606bb
Sai Vinaya <kumbamsaivinayareddy@gmail.com>
2026-02-23 10:00:00 +0530

    message

--- scope.py
+++ scope.py
@@ -7,3 +7,7 @@ def myfunc():
   print(x)
 
 myfunc()
+"""Global Scope
+A variable created in the main body of the Python code is a global variable and belongs to the global scope.
+
+Global variables are available from within any scope, global and local."""
