214232871768bf03ad45ab9067467663a2efa17b
Sai Vinaya <kumbamsaivinayareddy@gmail.com>
2026-02-26 10:00:00 +0530

    message

--- recursions.py
+++ recursions.py
@@ -2,3 +2,13 @@
 Recursion is when a function calls itself.
 
 Recursion is a common mathematical and programming concept. It means that a function calls itself. This has the benefit of meaning that you can loop through data to reach a result"""
+# A simple recursive function that counts down from 5:
+
+def countdown(n):
+  if n <= 0:
+    print("Done!")
+  else:
+    print(n)
+    countdown(n - 1)
+
+countdown(5)
