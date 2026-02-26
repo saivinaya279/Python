9fb2802cd853e9f941e2f9a88ae19a158a574e68
Sai Vinaya <kumbamsaivinayareddy@gmail.com>
2026-02-26 10:00:00 +0530

    message

---
+++ recursions.py
@@ -0,0 +1,4 @@
+"""Recursion
+Recursion is when a function calls itself.
+
+Recursion is a common mathematical and programming concept. It means that a function calls itself. This has the benefit of meaning that you can loop through data to reach a result"""
