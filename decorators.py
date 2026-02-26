2a5f8409455a12dadfd8e403fc44131a505fa147
Sai Vinaya <kumbamsaivinayareddy@gmail.com>
2026-02-26 17:57:49 +0530

    Daily python practice

---
+++ decorators.py
@@ -0,0 +1,40 @@
+"""Basic Decorator:
+Decorators let you add extra behavior to a function, without changing the function's code
+  A decorator is a function thaat takes another fun as input and returns a new function"""
+#   Basic Decorator
+"""Decorators are used to:
+
+✅ Add logging
+
+✅ Measure execution time
+
+✅ Add authentication
+
+✅ Validate inputs"""
+"""Imagine you wrote a function:
+
+👉 It just prints Hello
+
+But before it runs you want to print
+“Function started”
+
+And after it runs you want to print
+“Function ended”
+
+You don’t want to edit the original function every time.
+
+So you create a wrapper that adds extra behavior.
+
+That wrapper = decorator"""
+
+
+# normal function 
+def greet():
+    print("Hello")
+greet()
+# We create a wrapper function
+"""We want:
+
+Function started
+Hello
+Function ended"""
