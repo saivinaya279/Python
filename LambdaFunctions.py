761b6e91797f660446de0f2495226ec962cfffda
Sai Vinaya <kumbamsaivinayareddy@gmail.com>
2026-03-03 10:00:00 +0530

    message

---
+++ LambdaFunctions.py
@@ -0,0 +1,5 @@
+# Python Lambda Functions
+""" Lambda functions are small anonymous functions, meaning they do not have a defined name."""
+a = 'GeeksforGeeks'
+upper = lambda x: x.upper()  
+print(upper(a))
