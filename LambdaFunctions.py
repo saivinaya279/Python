e260b0c3d37bdf28366b1e364740340658b898db
Sai Vinaya <kumbamsaivinayareddy@gmail.com>
2026-03-04 10:00:00 +0530

    message

--- LambdaFunctions.py
+++ LambdaFunctions.py
@@ -3,3 +3,10 @@
 a = 'GeeksforGeeks'
 upper = lambda x: x.upper()  
 print(upper(a))
+# use cases
+""". Using with Condition Checking
+A lambda function can use conditional expressions (if-else) to return different results based on a condition."""
+check = lambda x: "Positive" if x > 0 else "Negative" if x < 0 else "Zero"
+print(check(5))   
+print(check(-3))  
+print(check(0))
