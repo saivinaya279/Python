9e59ced26759e85e6a52546530eae17d5257ff7b
Sai Vinaya <kumbamsaivinayareddy@gmail.com>
2026-03-12 22:38:12 +0530

    Update arrays.py

--- arrays.py
+++ arrays.py
@@ -114,3 +114,11 @@ for i in range(len(arr)-1):
     arr[i] = arr[i+1]
 arr[-1] = first
 print(arr)
+
+# Move elements one position to the right
+arr = [1,2,3,4,5]
+last = arr[-1]
+for i in range(len(arr)-1,0,-1):
+    arr[i] = arr[i-1]
+arr[0] = last
+print(arr)
