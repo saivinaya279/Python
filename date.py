0fe2cadc7494743d0e8ad86f8820e9fcc338ac25
Sai Vinaya <kumbamsaivinayareddy@gmail.com>
2026-03-13 10:00:00 +0530

    message

--- date.py
+++ date.py
@@ -16,3 +16,29 @@ x = datetime.datetime.now()
 print(x.year)
 print(x.month)
 print(x.day)
+# Creating a custom date
+import datetime
+
+x = datetime.datetime(2025,5,10)
+
+print(x)
+# Formatting dates (very important)
+
+# Using strftime()
+
+import datetime
+
+x = datetime.datetime.now()
+
+print(x.strftime("%Y"))  # Year
+print(x.strftime("%B"))  # Month name
+print(x.strftime("%A"))  # Day name
+# Date difference (important in data analysis)
+import datetime
+
+d1 = datetime.date(2025,5,1)
+d2 = datetime.date(2025,5,10)
+
+diff = d2 - d1
+
+print(diff.days)
