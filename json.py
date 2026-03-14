89eed9f6748d7ce52ed2512cb852208d23f192cf
Sai Vinaya <kumbamsaivinayareddy@gmail.com>
2026-03-14 10:00:00 +0530

    message

--- json.py
+++ json.py
@@ -1,3 +1,17 @@
 """# JSON in Python
 Python has a built-in package called json, which can be used to work with JSON data."""
 import json
+"""Parse JSON - Convert from JSON to Python
+If you have a JSON string, you can parse it by using the json.loads() method."""
+Convert from JSON to Python:
+
+import json
+
+# some JSON:
+x =  '{ "name":"John", "age":30, "city":"New York"}'
+
+# parse x:
+y = json.loads(x)
+
+# the result is a Python dictionary:
+print(y["age"])
