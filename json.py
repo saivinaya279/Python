1ca3f4e9d70a1a3ae27fcea68ce14060523fe498
Sai Vinaya <kumbamsaivinayareddy@gmail.com>
2026-03-15 10:00:00 +0530

    message

--- json.py
+++ json.py
@@ -15,3 +15,21 @@ y = json.loads(x)
 
 # the result is a Python dictionary:
 print(y["age"])
+"""Convert from Python to JSON
+If you have a Python object, you can convert it into a JSON string by using the json.dumps() method."""
+# Convert from Python to JSON:
+
+import json
+
+# a Python object (dict):
+x = {
+  "name": "John",
+  "age": 30,
+  "city": "New York"
+}
+
+# convert into JSON:
+y = json.dumps(x)
+
+# the result is a JSON string:
+print(y)
