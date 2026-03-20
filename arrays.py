d67e11c128f1c3e8ac27bfbfd7efd7b1d404f806
Sai Vinaya <kumbamsaivinayareddy@gmail.com>
2026-03-20 10:00:00 +0530

    message

--- arrays.py
+++ arrays.py
@@ -186,3 +186,13 @@ while left<=right:
     left+=1
     right-=1
 print(arr)
+def count_pairs(n):
+    count = 0
+    for a in range(1, n + 1):
+        for b in range(n + 1):
+            if a**3 + b**3 == n:
+                count += 1
+    return count
+
+n = 9
+print(count_pairs(n))
