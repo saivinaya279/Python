fe6ef80a384facd1e6f6c0f0256f4c72cf049462
Sai Vinaya <kumbamsaivinayareddy@gmail.com>
2026-03-28 10:00:00 +0530

    message

--- scope.py
+++ scope.py
@@ -1,41 +1,73 @@
-"""Local Scope
-A variable created inside a function belongs to the local scope of that function, and can only be used inside that function."""
-# A variable created inside a function is available inside that function:
+# """
+# """Local Scope
+# A variable created inside a function belongs to the local scope of that function, and can only be used inside that function."""
+# # A variable created inside a function is available inside that function:
 
-def myfunc():
-  x = 300
+# def myfunc():
+#   x = 300
+#   print(x)
+
+# myfunc()
+# """Global Scope
+# A variable created in the main body of the Python code is a global variable and belongs to the global scope.
+
+# Global variables are available from within any scope, global and local."""
+# # A variable created outside of a function is global and can be used by anyone:
+
+# x = 300
+
+# def myfunc():
+#   print(x)
+
+# myfunc()
+
+
+# print(x)
+# # naming Variables
+# # The function will print the local x, and then the code will print the global x:
+# x = 300
+
+# def myfunc():
+#   x = 200
+#   print(x)
+
+# myfunc()
+
+# print(x)
+# # Global Keyword
+# """f you need to create a global variable, but are stuck in the local scope, you can use the global keyword.
+
+# The global keyword makes the variable global."""   
+# """
+"""1️⃣ Local Scope """
+def fun():
+  x=10
   print(x)
+fun()
+# print(x)  Local variable cannot be accessed outside.
+"""def fun():
 
-myfunc()
-"""Global Scope
-A variable created in the main body of the Python code is a global variable and belongs to the global scope.
+    x = 10
 
-Global variables are available from within any scope, global and local."""
-# A variable created outside of a function is global and can be used by anyone:
+fun()
 
-x = 300
+print(x)""" # NameError: x not defined
+# 2️⃣ Global Scope
+x = 50   # global
 
-def myfunc():
-  print(x)
+def fun():
+    # x=20 ,error ,cannot modify  
+    print(x)
 
-myfunc()
+fun()
+x = 10
+# how to modify using global variable
+def fun():
 
+    global x
+
+    x = 20
+
+fun()
 
 print(x)
-# naming Variables
-# The function will print the local x, and then the code will print the global x:
-x = 300
-
-def myfunc():
-  x = 200
-  print(x)
-
-myfunc()
-
-print(x)
-# Global Keyword
-"""f you need to create a global variable, but are stuck in the local scope, you can use the global keyword.
-
-The global keyword makes the variable global."""
-# If you use the global keyword, the variable belongs to the global scope:
-
