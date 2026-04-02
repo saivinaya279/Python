0528183c3d1f62062020a91a012473c16111f199
Sai Vinaya <kumbamsaivinayareddy@gmail.com>
2026-04-02 10:00:00 +0530

    message

--- inheritance.py
+++ inheritance.py
@@ -15,3 +15,18 @@ d.sound()
 
 Create Person class with name and age.
 Create Student class with roll number."""
+class Person:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+class Student(Person):
+    def __init__(self,name,age,roll):
+        super().__init__(name,age)
+        self.roll = roll
+
+s = Student("Ravi",20,45)
+
+print(s.name)
+print(s.age)
+print(s.roll)
