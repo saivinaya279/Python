571bf19b4d7a963adef875d58d3a903df46c9316
Sai Vinaya <kumbamsaivinayareddy@gmail.com>
2026-04-05 10:00:00 +0530

    message

--- inheritance.py
+++ inheritance.py
@@ -69,3 +69,22 @@ class Dog(Animal):
 d = Dog()
 d.eat()
 d.bark()
+# Multilevel inheritance example
+
+class Animal:
+    def eat(self):
+        print("Eating")
+
+class Dog(Animal):
+    def bark(self):
+        print("Barking")
+
+class Puppy(Dog):
+    def weep(self):
+        print("Weeping")
+
+p = Puppy()
+
+p.eat()
+p.bark()
+p.weep()
