4d365a0834d4db1fdc3fd16a1121971010c32625
saivinaya279 <kumbamsaivinayareddy@gmail.com>
2026-04-27 20:00:00 +0530

    Contribution for 2026-04-27

---
+++ POLYMORPHISM.py
@@ -0,0 +1,98 @@
+"""Method overloading (Python way)
+
+Python does not support traditional overloading, but we simulate it.
+"""
+class Math:
+
+    def add(self,a,b,c=0):
+        print(a+b+c)
+
+m = Math()
+
+m.add(2,3)
+m.add(2,3,4)
+# Method overriding
+
+class Animal:
+    def sound(self):
+        print("Animal makes sound")
+
+class Dog(Animal):
+    def sound(self):
+        print("Dog barks")
+
+d = Dog()
+
+d.sound()
+# Inheritance one:
+class A:
+    def msg(self):
+        print("Hello")
+
+class B(A):
+    pass
+
+b = B()
+b.msg()
+# Polymorphism one:
+class A:
+    def show(self):
+        print("Class A")
+
+class B(A):
+    def show(self):
+        print("Class B")
+
+obj = B()
+obj.show()
+"""# Abstraction (Simple program)
+
+Question: Write a Python program using abstraction."""
+
+from abc import ABC, abstractmethod
+
+class Shape(ABC):
+
+    @abstractmethod
+    def area(self):
+        pass
+
+class Square(Shape):
+
+    def area(self):
+        print("Area calculated")
+
+obj = Square()
+obj.area()
+"""Function Polymorphism (Same function, different data)
+📌 Problem:
+
+Add numbers or strings"""
+
+def add(a, b):
+    return a + b
+
+print(add(2, 3))        
+print(add("Hi ", "You"))  
+"""Real-Life Style Problem (Very Important for Interviews)
+📌 Problem:
+
+Different payment methods"""
+
+class Payment:
+    def pay(self):
+        print("Processing payment")
+
+class UPI(Payment):
+    def pay(self):
+        print("Paid using UPI")
+
+class Card(Payment):
+    def pay(self):
+        print("Paid using Card")
+
+# polymorphism
+payments = [UPI(), Card()]
+
+for p in payments:
+    p.pay()
