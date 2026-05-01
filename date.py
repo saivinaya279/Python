f8716d0edff27c4d5d330270d5c103acc3d3c8a6
saivinaya279 <kumbamsaivinayareddy@gmail.com>
2026-05-01 20:00:00 +0530

    Updated project

--- date.py
+++ date.py
@@ -36,4 +36,9 @@ print(x.strftime("%A"))  # Day name
 # Date difference (important in data analysis)
 import datetime
 
+d1 = datetime.date(2025,5,1)
+d2 = datetime.date(2025,5,10)
 
+diff = d2 - d1
+
+print(diff.days)
--- dict.py
+++ dict.py
@@ -223,3 +223,11 @@ for n in nums:
     freq[n] = freq.get(n, 0) + 1
 
 print(freq)
+#  ✅ Code
+nums = [1, 2, 2, 3, 1]
+freq = {}
+
+for n in nums:
+    freq[n] = freq.get(n, 0) + 1
+
+print(freq)
