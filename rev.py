2d4a957b70e24942c0eb551c5b22787462955f96
saivinaya279 <kumbamsaivinayareddy@gmail.com>
2026-05-15 20:00:00 +0530

    Updated project

--- rev.py
+++ rev.py
@@ -549,11 +549,11 @@ swapping
 stop condition
 
 VERY important."""
-# arr=list(map(int,input().split()))
-# left=0
-# right=len(arr)-1
-# while left<right:
-#     arr[left],arr[right]=arr[right],arr[left]
-#     left+=1
-#     right-=1
-# print(arr)
+arr=list(map(int,input().split()))
+left=0
+right=len(arr)-1
+while left<right:
+    arr[left],arr[right]=arr[right],arr[left]
+    left+=1
+    right-=1
+print(arr)
