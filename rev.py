e6c139a2904784eba7de0f5672aa79abf27155e7
saivinaya279 <kumbamsaivinayareddy@gmail.com>
2026-05-26 20:00:00 +0530

    Updated project

--- rev.py
+++ rev.py
@@ -656,11 +656,3 @@ else:
     print("Not Palindrome")
 start = int(input("Start: "))
 end = int(input("End: "))
-
-for num in range(start, end + 1):
-    if num > 1:
-        for i in range(2, int(num**0.5) + 1):
-            if num % i == 0:
-                break
-        else:
-            print(num, end=" ")
