618d21d241558b5abe4bf958cd10d74f6577ff9a
saivinaya279 <kumbamsaivinayareddy@gmail.com>
2026-05-29 20:00:00 +0530

    Updated project

--- rev.py
+++ rev.py
@@ -664,3 +664,19 @@ for ch in text:
     freq[ch] = freq.get(ch, 0) + 1
 
 print(freq)
+start = 10
+end = 50
+
+for num in range(start, end + 1):
+    is_prime = True
+
+    if num < 2:
+        is_prime = False
+
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            is_prime = False
+            break
+
+    if is_prime:
+        print(num, end=" ")
