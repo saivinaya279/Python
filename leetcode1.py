c5f433cbb8a9637d301dd6dc4df89f576776d993
saivinaya279 <kumbamsaivinayareddy@gmail.com>
2026-06-06 20:00:00 +0530

    Updated project

--- leetcode1.py
+++ leetcode1.py
@@ -1,3 +1,10 @@
-nums=l
-for i in range(0,nums):
-    print(i)
+class Solution:
+    def sortColors(self, nums):
+        n=len(nums)
+        for i in range(n):
+            for j in range(n-i-1):
+                if nums[j]>nums[j+1]:
+                    nums[j],nums[j+1]=nums[j+1],nums[j]
+        return nums
+
+nums=list(map(int,input().split()))
