952f1d060d5fc20d7b29ae62cf889ec97e08b507
saivinaya279 <kumbamsaivinayareddy@gmail.com>
2026-06-07 20:00:00 +0530

    Updated project

--- leetcode1.py
+++ leetcode1.py
@@ -8,3 +8,15 @@ class Solution:
         return nums
 
 nums=list(map(int,input().split()))
+class Solution:
+    def largestNumber(self, nums):
+        n=len(nums)
+        for i in range(n):
+            for j in range(0,n-i-1):
+                if str(nums[j]) + str(nums[j + 1]) < str(nums[j + 1]) + str(nums[j]):
+                    nums[j],nums[j+1]=nums[j+1],nums[j]
+        ans = ''.join(map(str, nums))
+        if ans[0]=="0":
+            return "0"
+        return ans
+nums=list(map(int,input().split()))
