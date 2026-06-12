0439309be1a6879a431b8a7d6525f91b358522e8
saivinaya279 <kumbamsaivinayareddy@gmail.com>
2026-06-12 20:00:00 +0530

    Updated project

--- insertion.py
+++ insertion.py
@@ -8,3 +8,16 @@ for i in range(n):
             arr[j], arr[j + 1] = arr[j + 1], arr[j]
 
 print(arr)
+arr = [5, 2, 4, 1, 3]
+
+for i in range(1, len(arr)):
+    key = arr[i]
+    j = i - 1
+
+    while j >= 0 and arr[j] > key:
+        arr[j + 1] = arr[j]
+        j -= 1
+
+    arr[j + 1] = key
+
+print(arr)
