1b26669c0af1649edd719e0308487d4736e4dd97
saivinaya279 <kumbamsaivinayareddy@gmail.com>
2026-06-16 20:00:00 +0530

    Updated project

--- insertion.py
+++ insertion.py
@@ -58,3 +58,17 @@ if swapped:
     print("Not Sorted")
 else:
     print("Already Sorted")
+arr = [64, 25, 12, 22, 11]
+
+n = len(arr)
+
+for i in range(n):
+    min_idx = i
+
+    for j in range(i + 1, n):
+        if arr[j] < arr[min_idx]:
+            min_idx = j
+
+    arr[i], arr[min_idx] = arr[min_idx], arr[i]
+
+print(arr)
