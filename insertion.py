1f92bdaac87fe6900e4c2775e6c6cdedac3ae2f6
saivinaya279 <kumbamsaivinayareddy@gmail.com>
2026-06-18 20:00:00 +0530

    Updated project

--- insertion.py
+++ insertion.py
@@ -86,3 +86,17 @@ for i in range(n):
     print("Minimum:", arr[min_idx])
 
     arr[i], arr[min_idx] = arr[min_idx], arr[i]
+arr = ["mango", "apple", "banana"]
+
+n = len(arr)
+
+for i in range(n):
+    min_idx = i
+
+    for j in range(i + 1, n):
+        if arr[j] < arr[min_idx]:
+            min_idx = j
+
+    arr[i], arr[min_idx] = arr[min_idx], arr[i]
+
+print(arr)
