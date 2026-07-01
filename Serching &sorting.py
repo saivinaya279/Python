fb048adf96f8501dfb29a552f0f3af188df82b75
saivinaya279 <kumbamsaivinayareddy@gmail.com>
2026-07-01 20:00:00 +0530

    commit

--- Serching &sorting.py
+++ Serching &sorting.py
@@ -7,3 +7,23 @@ for i in range(len(arr)):
         break
 else:
     print(-1)
+arr = [10,20,30,40,50,60]
+
+key = 40
+
+low = 0
+high = len(arr)-1
+
+while low <= high:
+
+    mid = (low+high)//2
+
+    if arr[mid] == key:
+        print(mid)
+        break
+
+    elif arr[mid] < key:
+        low = mid + 1
+
+    else:
+        high = mid - 1
