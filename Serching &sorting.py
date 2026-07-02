3439fb46de79bbdc61b4f70814200a3ad78f579a
saivinaya279 <kumbamsaivinayareddy@gmail.com>
2026-07-02 20:00:00 +0530

    commit

--- Serching &sorting.py
+++ Serching &sorting.py
@@ -27,3 +27,23 @@ while low <= high:
 
     else:
         high = mid - 1
+arr = [5,4,3,2,1]
+
+n = len(arr)
+
+for i in range(n-1):
+
+    swapped = False
+
+    for j in range(n-1-i):
+
+        if arr[j] > arr[j+1]:
+
+            arr[j],arr[j+1]=arr[j+1],arr[j]
+
+            swapped=True
+
+    if not swapped:
+        break
+
+print(arr)
