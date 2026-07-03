f67aac2712500f571041c970e793bedc09d1b456
saivinaya279 <kumbamsaivinayareddy@gmail.com>
2026-07-03 20:00:00 +0530

    commit

--- Serching &sorting.py
+++ Serching &sorting.py
@@ -115,3 +115,20 @@ def merge_sort(arr):
             arr[k]=right[j]
             j+=1
             k+=1
+def partition(arr,low,high):
+
+    pivot=arr[high]
+
+    i=low-1
+
+    for j in range(low,high):
+
+        if arr[j]<pivot:
+
+            i+=1
+
+            arr[i],arr[j]=arr[j],arr[i]
+
+    arr[i+1],arr[high]=arr[high],arr[i+1]
+
+    return i+1
