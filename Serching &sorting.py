79b581b6bd6058be10b75c0082ad7d12ace30cb7
saivinaya279 <kumbamsaivinayareddy@gmail.com>
2026-07-04 20:00:00 +0530

    commit

--- Serching &sorting.py
+++ Serching &sorting.py
@@ -64,3 +64,20 @@ for i in range(n):
     arr[i],arr[minimum]=arr[minimum],arr[i]
 
 print(arr)
+arr=[5,2,4,6,1,3]
+
+for i in range(1,len(arr)):
+
+    key=arr[i]
+
+    j=i-1
+
+    while j>=0 and arr[j]>key:
+
+        arr[j+1]=arr[j]
+
+        j-=1
+
+    arr[j+1]=key
+
+print(arr)
