aeb37327c6e483f2fc6a92c6b40eb0b7911bb2d3
saivinaya279 <kumbamsaivinayareddy@gmail.com>
2026-07-06 20:00:00 +0530

    commit

---
+++ bubble_sort.py
@@ -0,0 +1,19 @@
+def bubble_sort(arr):
+    n = len(arr)
+
+    for i in range(n - 1):
+        swapped = False
+
+        for j in range(n - 1 - i):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                swapped = True
+
+        if not swapped:
+            break
+
+    return arr
+
+
+arr = [5, 1, 4, 2, 8]
+print(bubble_sort(arr))
