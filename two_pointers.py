61b4d3d12c76edf39a971ef707a5ea0f3de4cda2
saivinaya279 <kumbamsaivinayareddy@gmail.com>
2026-07-18 20:00:00 +0530

    commit

--- two_pointers.py
+++ two_pointers.py
@@ -19,3 +19,27 @@ while left<right:
 
     else:
         right-=1
+
+
+def is_pal(s):
+    left = 0
+
+    right = len(s)-1
+
+    while left<right:
+        if s[left]!=s[right]:
+            return False
+        else:
+            left+=1
+            right-=1
+
+    return True
+
+
+
+s = input()
+if is_pal(s):
+    print("palindrome")
+
+else:
+    print("not a palindrome")
