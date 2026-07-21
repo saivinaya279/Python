8dc94bd7334bd4ea4e80916c07adbe93b326a590
saivinaya279 <kumbamsaivinayareddy@gmail.com>
2026-07-21 20:00:00 +0530

    commit

--- two_pointers.py
+++ two_pointers.py
@@ -95,3 +95,14 @@ def two_sum_sorted(arr, target):
             right -= 1            
 
     return None 
+def remove_duplicates(arr):
+    if not arr:
+        return 0
+
+    slow = 0
+    for fast in range(1, len(arr)):
+        if arr[fast] != arr[slow]:
+            slow += 1
+            arr[slow] = arr[fast]
+
+    return slow + 1  
