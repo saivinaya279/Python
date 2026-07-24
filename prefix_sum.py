dabb9301f5fc07b366a0c7e793e35dc31c7304ce
saivinaya279 <kumbamsaivinayareddy@gmail.com>
2026-07-24 20:00:00 +0530

    commit

--- prefix_sum.py
+++ prefix_sum.py
@@ -17,3 +17,27 @@ def range_sum(prefix, left, right):
 arr = [2, 4, 1, 5, 3]
 prefix = build_prefix_sum(arr)
 print(range_sum(prefix, 1, 3))   
+def build_2d_prefix(matrix):
+    rows, cols = len(matrix), len(matrix[0])
+    prefix = [[0] * (cols + 1) for _ in range(rows + 1)]
+
+    for i in range(1, rows + 1):
+        for j in range(1, cols + 1):
+            prefix[i][j] = (matrix[i-1][j-1]
+                             + prefix[i-1][j]
+                             + prefix[i][j-1]
+                             - prefix[i-1][j-1])
+    return prefix
+
+
+def range_sum_2d(prefix, r1, c1, r2, c2):
+    # sum of matrix[r1..r2][c1..c2] inclusive
+    return (prefix[r2+1][c2+1]
+            - prefix[r1][c2+1]
+            - prefix[r2+1][c1]
+            + prefix[r1][c1])
+
+
+matrix = [[1,2,3],[4,5,6],[7,8,9]]
+prefix = build_2d_prefix(matrix)
+print(range_sum_2d(prefix, 1, 1, 2, 2))   # 28
