0fcc80192ba68f03dd4a05a3a17ec0684ea9d275
saivinaya279 <kumbamsaivinayareddy@gmail.com>
2026-07-28 18:00:00 +0530

    Initial project setup and folder structure

--- stack.py
+++ stack.py
@@ -1,59 +1,17 @@
-class Stack:
-    def __init__(self):
-        self.stack=[]
-    def push(self,ele):
-        self.stack.append(ele)
-    def pop(self):
-        if self.isEmpty():
-            return "Stack is empty"
-        return self.stack.pop()
-    def peek(self):
-        if self.isEmpty():
-            return "empty"
-        return self.stack[-1]
-    def isEmpty(self):
-        return len(self.stack)==0
-    def size(self):
-        return len(self.stack)
-obj_st = Stack()
-obj_st.push(1)
-obj_st.push(2)
-obj_st.push(3)
-print("stack:", obj_st.stack)
-print("pop:", obj_st.pop())
-print("after pop-stack:", obj_st.stack)
-class Stack:
-    def __init__(self):
-        self.stack = []
+stack = []
 
-    def push(self, ele):
-        self.stack.append(ele)
+stack.append(10)
+stack.append(20)
+stack.append(30)
 
-    def pop(self):
-        if self.isEmpty():
-            return "Stack is empty"
-        return self.stack.pop()
+print("Stack:", stack)
+stack = []
 
-    def peek(self):
-        if self.isEmpty():
-            return "Stack is empty"
-        return self.stack[-1]
+def push(value):
+    stack.append(value)
 
-    def isEmpty(self):
-        return len(self.stack) == 0
+push(10)
+push(20)
+push(30)
 
-    def size(self):
-        return len(self.stack)
-
-
-obj_st = Stack()
-obj_st.push(1)
-obj_st.push(2)
-obj_st.push(3)
-
-print("stack:", obj_st.stack)
-print("pop:", obj_st.pop())
-print("after pop-stack:", obj_st.stack)
-print("peek:", obj_st.peek())
-print("isempty:", obj_st.isEmpty())
-print("size:", obj_st.size())
+print(stack)
